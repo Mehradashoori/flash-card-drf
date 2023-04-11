9d0914ca8dba71fcf30c68e42633f277d52a48d4
Mehrad <mehradashoori456@gmail.com>
2023-04-11 13:33:58 +0430

    Add CRUD api for flash card

---
+++ card/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from card import views
+
+urlpatterns = [
+    path('create/', views.CreateFlashCardView.as_view(), name='create-flash-card'),
+    path('update/<id>/', views.UpdateFlashCardView.as_view(), name='update-flash-card'),
+    path('delete/<id>/', views.DeleteFlashCardView.as_view(), name='delete-flash-card'),
+    path('list/<user_id>/', views.ListFlashCardsview.as_view(), name='list-flash-card'),
+
+]
+
+
